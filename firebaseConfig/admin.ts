7ff5597042aad44f14abe470f515ddeb3d16e350
Tuncay Huseynov <thuseynov2020@ada.edu.az>
2022-07-06 03:34:30 +0400

    trying to fix firebase admin issue in production

--- firebaseConfig/admin.ts
+++ firebaseConfig/admin.ts
@@ -2,17 +2,19 @@ import admin from "firebase-admin";
 
 
 export const adminApp = admin.apps.length === 0 ? admin.initializeApp({
-    credential: admin.credential.cert({
-        "type": "service_account",
-        "project_id": "remox-dao",
-        "private_key_id": process.env.PRIVATE_KEY_ID,
-        "private_key": process.env.PRIVATE_KEY,
-        "client_email": process.env.CLIENT_EMAIL,
-        "client_id": process.env.CLIENT_ID,
-        "auth_uri": process.env.AUTH_URI,
-        "token_uri": process.env.TOKEN_URI,
-        "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-        "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
-    } as any),
+    credential: admin.credential.cert(JSON.stringify(
+        {
+            "type": "service_account",
+            "project_id": "remox-dao",
+            "private_key_id": process.env.PRIVATE_KEY_ID,
+            "private_key": process.env.PRIVATE_KEY,
+            "client_email": process.env.CLIENT_EMAIL,
+            "client_id": process.env.CLIENT_ID,
+            "auth_uri": process.env.AUTH_URI,
+            "token_uri": process.env.TOKEN_URI,
+            "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+            "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL
+        }
+    )),
     databaseURL: "https://remox-dao-default-rtdb.firebaseio.com"
 }, "admin") : admin.app("admin");
