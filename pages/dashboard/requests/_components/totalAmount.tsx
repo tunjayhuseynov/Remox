70ed64c4d94dc92f53cd0f96e43fab6c2af301e5
rijatalizada <rijatga@code.edu.az>
2022-09-09 14:50:51 +0400

    RequestFinalizing

--- pages/dashboard/requests/_components/totalAmount.tsx
+++ pages/dashboard/requests/_components/totalAmount.tsx
@@ -5,27 +5,28 @@ import { IMember } from 'types/dashboard/contributors';
 import { SelectCurrencies, SelectTotalBalance } from "redux/slices/account/selector";
 import { AltCoins, Coins } from "types";
 import { SetComma } from 'utils'
+import { useWalletKit } from "hooks";
 
-export const TotalUSDAmount = (coinList: (IRequest | IMember)[], currency: Coins) => {
+export const TotalUSDAmount = (coinList: (IRequest | IMember)[], Coins: Coins) => {
     return coinList.reduce((acc, curr) => {
-        const coin = Object.entries(currency).find(c => c[0] === curr.currency) as [string, AltCoins] | undefined
+        const coin = Object.values(Coins).find((c) => c.name === curr.currency)
         if (coin) {
             const amount = typeof curr.amount === "string" ? parseFloat(curr.amount) : curr.amount
 
             if (curr.usdBase) {
                 acc + amount
             } else {
-                acc += ((coin[1]?.priceUSD ?? 1) * amount)
+                acc += ((coin?.priceUSD ?? 1) * amount)
             }
 
             if (curr.secondaryCurrency && curr.secondaryAmount) {
-                const secondaryCoin = Object.entries(currency).find(c => c[0] === curr.secondaryCurrency) as [string, AltCoins] | undefined
+                const secondaryCoin = Object.values(Coins).find((c) => c.name === curr.secondaryCurrency)
                 if (secondaryCoin) {
                     const secondaryAmount = typeof curr.secondaryAmount === "string" ? parseFloat(curr.secondaryAmount) : curr.secondaryAmount
                     if (curr.usdBase) {
                         acc + secondaryAmount
                     } else {
-                        acc += ((secondaryCoin[1]?.priceUSD ?? 1) * secondaryAmount)
+                        acc += ((secondaryCoin?.priceUSD ?? 1) * secondaryAmount)
                     }
                 }
             }
@@ -35,65 +36,30 @@ export const TotalUSDAmount = (coinList: (IRequest | IMember)[], currency: Coins
 }
 
 
-// export default function TotalAmount({ coinList }: { coinList: IRequest[] | IMember[] }) {
-
-//     const totalBalance = useSelector(SelectTotalBalance)
-//     const currency = useSelector(SelectCurrencies)
-
-//     const totalAmount = useMemo<number>(() => TotalUSDAmount(coinList, currency), [coinList, currency])
-
-//     return <>
-//         <div className={`pb-4 ${totalAmount.toFixed(2) !== "0.00" && "border-b"} w-full flex flex-col justify-center items-start space-y-3`}>
-//             <div className="flex justify-start   items-center">
-//                 <div className="font-bold  text-2xl">${totalBalance?.toFixed(2)} USD</div>
-//             </div>
-//             {totalAmount.toFixed(2) !== "0.00" &&
-//                 <div className="flex justify-end items-center">
-//                     <div>-{totalAmount.toFixed(2)} USD</div>
-//                 </div>}
-//         </div>
-//         {totalAmount.toFixed(2) !== "0.00" && <div className="pt-2 ">
-//             <div className="flex justify-start items-center">
-//                 <div className="font-bold text-2xl">{((totalBalance ?? 0) - totalAmount).toFixed(2)} USD</div>
-//             </div>
-//         </div>}
-
-//     </>;
-// }
-
 export default function TotalAmount({ coinList }: { coinList: IRequest[] | IMember[] }) {
 
     const totalBalance = useSelector(SelectTotalBalance)
     const currency = useSelector(SelectCurrencies)
+    const { GetCoins } = useWalletKit();
+    const totalAmount = useMemo<number>(() => TotalUSDAmount(coinList, GetCoins ), [coinList, currency])
 
-    console.log("coinList")
-    console.log(coinList);
-
-    const totalAmount = useMemo<number>(() => TotalUSDAmount(coinList, currency), [coinList, currency])
-
-    console.log("totalAmount")
-    console.log(totalAmount);
-
-    
-
-    // const totalAmount = useMemo<number>(() => TotalUSDAmount(coinList, currency), [coinList, currency])
     return <>
         <div className={`mb-4 w-full  ${coinList.length > 0 && "border-r border-greylish dark:border-[#454545]  border-opacity-10"} w-full flex flex-col justify-center  pr-5`}>
             <div className="w-full flex justify-start   items-center">
                 <div className={`font-semibold   text-xl`}>${SetComma(totalBalance)}</div>
             </div>
-            {/* {totalAmount.toFixed(2) !== "0.00" &&
+            {totalAmount.toFixed(2) !== "0.00" &&
                 <div className="w-full flex justify-start items-center text-lg font-semibold">
-                    <div>${SetComma(totalAmount)}-</div>
+                    <div>-${SetComma(totalAmount)}</div>
                 </div>
-            } */}
+            }
         </div>
-        {/* {totalAmount.toFixed(2) !== "0.00" &&  
+        {totalAmount.toFixed(2) !== "0.00" &&  
             <div className="w-full pt-2  pr-10">
                 <div className="w-full flex justify-start items-center">
-                    <div className="font-semibold text-xl">${SetComma(((1531070 ?? 0) - totalAmount))}</div>
+                    <div className="font-semibold text-xl">${SetComma((totalBalance - totalAmount))}</div>
                 </div>
             </div>
-        } */}
+        }
     </>
 }
