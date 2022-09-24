ff0772b24f00cc73f4baa1e08f6e5c909d9bf62f
rijatalizada <rijatga@code.edu.az>
2022-09-25 03:16:31 +0400

    Added Pagination to Contributors, Swap edited

--- hooks/walletSDK/useSwap.tsx
+++ hooks/walletSDK/useSwap.tsx
@@ -55,68 +55,66 @@ export default function useSwap() {
 
                 setLoading(false)
                 return { minimumAmountOut: minOut, oneTokenValue: new BigNumber(routeOne.routesInfos[0].outAmount.toString()).dividedBy(10 ** outputCoin.decimals!).toString(), feeAmount: fee ?? "0" }
-            }
-
-
-            const provider = new CeloProvider('https://forno.celo.org')
-            let priceImpactWithoutFeePercent;
-            let feeAmount: any = 0;
-
-            let inputAddress = inputCoin.address;
-            const input = await Fetcher.fetchTokenData(ChainId.MAINNET, getAddress(inputAddress), provider);
-
-            let outputAddress = outputCoin.address;
-            const output = await Fetcher.fetchTokenData(ChainId.MAINNET, getAddress(outputAddress), provider);
-
-            const pair = await Fetcher.fetchPairData(output, input, provider);
-            const route = new Route([pair], input);
-
-            let minimumAmountOut = "0";
-
-            if (parseFloat(amount) > 0) {
-                const amountIn = toWei(amount)
-                const trade = new Trade(route, new TokenAmount(input, amountIn), TradeType.EXACT_INPUT); //
-
-
-                const BASE_FEE = new Percent(UbeJSBI.BigInt(30), UbeJSBI.BigInt(10000))
-                const ONE_HUNDRED_PERCENT = new Percent(UbeJSBI.BigInt(10000), UbeJSBI.BigInt(10000))
-                const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE)
-
-                const realizedLPFee = !trade
-                    ? undefined
-                    : ONE_HUNDRED_PERCENT.subtract(
-                        trade.route.pairs.reduce<Fraction>(
-                            (currentFee: Fraction): Fraction => currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
-                            ONE_HUNDRED_PERCENT
+            } else if (blockchain.name === "celo"){
+                const provider = new CeloProvider('https://forno.celo.org')
+                let priceImpactWithoutFeePercent;
+                let feeAmount: any = 0;
+                
+                let inputAddress = inputCoin.address;
+                const input = await Fetcher.fetchTokenData(ChainId.MAINNET, getAddress(inputAddress), provider);
+                
+                let outputAddress = outputCoin.address;
+                const output = await Fetcher.fetchTokenData(ChainId.MAINNET, getAddress(outputAddress), provider);
+                
+                const pair = await Fetcher.fetchPairData(output, input, provider);
+                const route = new Route([pair], input);
+                
+                let minimumAmountOut = "0";
+                
+                if (parseFloat(amount) > 0) {
+                    const amountIn = toWei(amount)
+                    const trade = new Trade(route, new TokenAmount(input, amountIn), TradeType.EXACT_INPUT); //
+                
+                
+                    const BASE_FEE = new Percent(UbeJSBI.BigInt(30), UbeJSBI.BigInt(10000))
+                    const ONE_HUNDRED_PERCENT = new Percent(UbeJSBI.BigInt(10000), UbeJSBI.BigInt(10000))
+                    const INPUT_FRACTION_AFTER_FEE = ONE_HUNDRED_PERCENT.subtract(BASE_FEE)
+                
+                    const realizedLPFee = !trade
+                        ? undefined
+                        : ONE_HUNDRED_PERCENT.subtract(
+                            trade.route.pairs.reduce<Fraction>(
+                                (currentFee: Fraction): Fraction => currentFee.multiply(INPUT_FRACTION_AFTER_FEE),
+                                ONE_HUNDRED_PERCENT
+                            )
                         )
-                    )
-
-                const priceImpactWithoutFeeFraction = trade && realizedLPFee ? trade.priceImpact.subtract(realizedLPFee) : undefined
-
-                priceImpactWithoutFeePercent = priceImpactWithoutFeeFraction
-                    ? new Percent(priceImpactWithoutFeeFraction.numerator, priceImpactWithoutFeeFraction.denominator)
-                    : undefined
-
-                feeAmount =
-                    realizedLPFee &&
-                    trade &&
-                    new TokenAmount(trade.inputAmount.token, realizedLPFee.multiply(trade.inputAmount.raw).quotient)
-
-                feeAmount = feeAmount.toSignificant(4)
-                const amountOut = trade.minimumAmountOut(new Percent(slippage.toString(), '1000'));
-                minimumAmountOut = amountOut.raw.toString();
-                minimumAmountOut = fromWei(minimumAmountOut);
-            }
-
-            const oneCoin = toWei(1);
-            const tradeOne = new Trade(route, new TokenAmount(input, oneCoin), TradeType.EXACT_INPUT); //
-
-            const amountOutOne = tradeOne.minimumAmountOut(new Percent(slippage, '1000'));
-            let minimumAmountOutOne = amountOutOne.raw.toString();
-            minimumAmountOutOne = fromWei(minimumAmountOutOne);
-
-            setLoading(false)
-            return { minimumAmountOut, oneTokenValue: minimumAmountOutOne, feeAmount: feeAmount };
+                            
+                    const priceImpactWithoutFeeFraction = trade && realizedLPFee ? trade.priceImpact.subtract(realizedLPFee) : undefined
+                            
+                    priceImpactWithoutFeePercent = priceImpactWithoutFeeFraction
+                        ? new Percent(priceImpactWithoutFeeFraction.numerator, priceImpactWithoutFeeFraction.denominator)
+                        : undefined
+                            
+                    feeAmount =
+                        realizedLPFee &&
+                        trade &&
+                        new TokenAmount(trade.inputAmount.token, realizedLPFee.multiply(trade.inputAmount.raw).quotient)
+                            
+                    feeAmount = feeAmount.toSignificant(4)
+                    const amountOut = trade.minimumAmountOut(new Percent(slippage.toString(), '1000'));
+                    minimumAmountOut = amountOut.raw.toString();
+                    minimumAmountOut = fromWei(minimumAmountOut);
+                }
+            
+                const oneCoin = toWei(1);
+                const tradeOne = new Trade(route, new TokenAmount(input, oneCoin), TradeType.EXACT_INPUT); //
+            
+                const amountOutOne = tradeOne.minimumAmountOut(new Percent(slippage, '1000'));
+                let minimumAmountOutOne = amountOutOne.raw.toString();
+                minimumAmountOutOne = fromWei(minimumAmountOutOne);
+                setLoading(false)
+                return { minimumAmountOut, oneTokenValue: minimumAmountOutOne, feeAmount: feeAmount };
+            }    
         } catch (e: any) {
             setLoading(false)
             throw new Error(e);
