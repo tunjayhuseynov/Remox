0400ee14e273f9478aac445519cb00531056bbc4
Tuncay Huseynov <thuseynov2020@ada.edu.az>
2022-09-30 07:48:54 +0400

    production mode testing wt some fixes

--- pages/api/payments/send/_solana.ts
+++ pages/api/payments/send/_solana.ts
@@ -1,8 +1,8 @@
 import { Connection, Keypair, PublicKey, TransactionInstruction } from "@solana/web3.js"
-import { WITHDRAW_TOKEN_STRING, ZEBEC_PROGRAM_ID } from "@zebec-protocol/stream/src/constants"
+// import { WITHDRAW_TOKEN_STRING, ZEBEC_PROGRAM_ID } from "@zebec-protocol/stream/src/constants"
 import { SolanaSerumEndpoint } from "components/Wallet"
 import { token } from "easy-spl"
-import * as INSTRUCTIONS from "@zebec-protocol/stream/src/instructions";
+// import * as INSTRUCTIONS from "@zebec-protocol/stream/src/instructions";
 import { adminApp } from "firebaseConfig/admin"
 import { ISwap } from "./index.api"
 import { Jupiter } from "@jup-ag/core"
@@ -50,46 +50,47 @@ export async function solanaInstructions(coins: Coins, accountId: string, public
         const recipientAddress = new PublicKey(recipient);
         const tokenMintAddress = new PublicKey(solanaCoin.address);
 
-        const [withdrawEscrowAddress, _] = await PublicKey.findProgramAddress(
-            [Buffer.from(WITHDRAW_TOKEN_STRING), senderAddress.toBuffer(), tokenMintAddress.toBuffer()],
-            new PublicKey(ZEBEC_PROGRAM_ID)
-        )
+        // const [withdrawEscrowAddress, _] = await PublicKey.findProgramAddress(
+        //     [Buffer.from(WITHDRAW_TOKEN_STRING), senderAddress.toBuffer(), tokenMintAddress.toBuffer()],
+        //     new PublicKey(ZEBEC_PROGRAM_ID)
+        // )
 
-        const escrowAddress = new Keypair();
+        // const escrowAddress = new Keypair();
 
-        const ix = await INSTRUCTIONS.createInitMultiTokenStreamInstruction(
-            senderAddress,
-            recipientAddress,
-            escrowAddress.publicKey,
-            withdrawEscrowAddress,
-            new PublicKey(ZEBEC_PROGRAM_ID),
-            tokenMintAddress,
-            start_time,
-            end_time,
-            amount
-        )
+        // const ix = await INSTRUCTIONS.createInitMultiTokenStreamInstruction(
+        //     senderAddress,
+        //     recipientAddress,
+        //     escrowAddress.publicKey,
+        //     withdrawEscrowAddress,
+        //     new PublicKey(ZEBEC_PROGRAM_ID),
+        //     tokenMintAddress,
+        //     start_time,
+        //     end_time,
+        //     amount
+        // )
 
-        await adminApp.firestore().collection("recurring").add({
-            taskId: escrowAddress.publicKey.toBase58(),
-            sender: senderAddress.toBase58(),
-            recipient: recipientAddress.toBase58(),
-            blockchain: "solana",
-            protocol: "zebec",
-            from: start_time,
-            to: end_time,
-            accountId,
-            interval: null,
-            inputs: [
-                {
-                    amount,
-                    coin,
-                    recipient
-                }
-            ],
+        // await adminApp.firestore().collection("recurring").add({
+        //     taskId: escrowAddress.publicKey.toBase58(),
+        //     sender: senderAddress.toBase58(),
+        //     recipient: recipientAddress.toBase58(),
+        //     blockchain: "solana",
+        //     protocol: "zebec",
+        //     from: start_time,
+        //     to: end_time,
+        //     accountId,
+        //     interval: null,
+        //     inputs: [
+        //         {
+        //             amount,
+        //             coin,
+        //             recipient
+        //         }
+        //     ],
 
-        } as ITasking)
+        // } as ITasking)
 
-        return [ix];
+        // return [ix];
+        return []
     }
 
     return await token.transferTokenInstructions(connection, new PublicKey(solanaCoin.address), from, to, amount)
