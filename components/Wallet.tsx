b919649d8d5f89131033383a1ca61962aaf607f8
Tuncay Huseynov <thuseynov2020@ada.edu.az>
2022-10-17 19:30:01 +0300

    somae changes

--- components/Wallet.tsx
+++ components/Wallet.tsx
@@ -16,17 +16,7 @@ import {
 import { BASE_URL } from 'utils/api';
 import { CeloProvider, NetworkNames } from '@celo/react-celo';
 import { Blockchains } from 'types/blockchains';
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-    getDefaultWallets,
-    RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import {
-    chain,
-    configureChains,
-    createClient,
-    WagmiConfig,
-} from 'wagmi';
+
 import { publicProvider } from 'wagmi/providers/public';
 
 
@@ -40,42 +30,42 @@ export default function Wallet({ children }: { children: JSX.Element }) {
     // const web3React = useWeb3React()
     const SolNetwork = WalletAdapterNetwork.Mainnet;
 
-    const { chains, provider } = configureChains(
-        [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, {
-            id: 42220,
-            name: 'Celo',
-            network: 'mainnet',
-            rpcUrls: {
-                default: CeloEndpoint
-            },
-            nativeCurrency: {
-                decimals: 18,
-                name: 'CELO',
-                symbol: 'CELO',
-            },
-            blockExplorers: {
-                default: {
-                    name: 'Celo Explorer',
-                    url: 'https://explorer.celo.org',
-                },
-            },
-            testnet: false,
-        }],
-        [
-            publicProvider()
-        ]
-    );
+    // const { chains, provider } = configureChains(
+    //     [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum, {
+    //         id: 42220,
+    //         name: 'Celo',
+    //         network: 'mainnet',
+    //         rpcUrls: {
+    //             default: CeloEndpoint
+    //         },
+    //         nativeCurrency: {
+    //             decimals: 18,
+    //             name: 'CELO',
+    //             symbol: 'CELO',
+    //         },
+    //         blockExplorers: {
+    //             default: {
+    //                 name: 'Celo Explorer',
+    //                 url: 'https://explorer.celo.org',
+    //             },
+    //         },
+    //         testnet: false,
+    //     }],
+    //     [
+    //         publicProvider()
+    //     ]
+    // );
 
-    const { connectors } = getDefaultWallets({
-        appName: 'Remox App',
-        chains
-    });
+    // const { connectors } = getDefaultWallets({
+    //     appName: 'Remox App',
+    //     chains
+    // });
 
-    const wagmiClient = createClient({
-        autoConnect: true,
-        connectors,
-        provider
-    })
+    // const wagmiClient = createClient({
+    //     autoConnect: true,
+    //     connectors,
+    //     provider
+    // })
 
     // const endpoint = useMemo(() => clusterApiUrl(SolNetwork), [SolNetwork]);
 
